5d55d5ed97e707f30454aec66b67d0f25370de4d
Neil Enns <{ID}+{username}@users.noreply.github.com>
2022-12-07 05:33:20 -0800

    Dual displays and AAU1 support

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import React, { useState, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { McduScreen } from './McduScreen.jsx';
 import { WebsocketContext } from './WebsocketContext.jsx';
+import querystring from 'query-string';
 
 function App() {
+    var query = querystring.parse(global.location.search);
+    let requestedId = query.screen;
+
     const [fullscreen, setFullscreen] = useState(window.location.href.endsWith('fullscreen'));
+    const [screenId, setScreenId] = useState(requestedId);
+
     const socketUrl = `ws://localhost:8088`;
 
     const [content, setContent] = useState(
         {
+            id: 1,
             lines: [
                 ['', '', ''],
                 ['', '', ''],
@@ -23,13 +30,8 @@ function App() {
                 ['', '', ''],
                 ['', '', ''],
                 ['', '', ''],
+                ['', '', ''],
             ],
-            scratchpad: '',
-            message: '',
-            title: '',
-            titleLeft: '',
-            page: '',
-            exec: false,
             power: false,
         },
     );
@@ -54,10 +56,26 @@ function App() {
         if (lastMessage != null) {
             const messageType = lastMessage.data.split(':')[0];
             if (messageType === 'update') {
-                setContent(JSON.parse(lastMessage.data.substring(lastMessage.data.indexOf(':') + 1)).left);
+                const jsonIn = JSON.parse(lastMessage.data.substring(lastMessage.data.indexOf(':') + 1));
+                const screenName = screenId == 2 ? 'right' : 'left';
+                const newContent = jsonIn[screenName];
+                if (newContent) {
+                    newContent.id = screenId;
+                    setContent(newContent);
+                }
             }
         }
     }, [lastMessage]);
+
+    function changeCdu(screen) {
+        if (screen == screenId) {
+            return;
+        }
+        setScreenId(screen);
+        if (readyState === ReadyState.OPEN) {
+            sendMessage('requestUpdate');
+        }        
+    }
 
     return (
         <div className="fullscreen">
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { Server, OPEN } from 'ws';
 import WinState from 'electron-win-state'
 import * as Store from 'electron-store';
 
+var windows = new Set();
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
@@ -109,36 +111,47 @@ const startSockets = () => {
   });
 }
 
-const createWindow = () => {
+// Information on creating mulitple windows comes from https://spin.atomicobject.com/2021/07/08/multiple-windows-electron-app/.
+const createWindows = () => {
+  createWindow(1);
+  createWindow(2);
+}
+
+const createWindow = (index) => {
   // Create the browser window.
-  const mainWindow = WinState.createBrowserWindow({
+  var window = WinState.createBrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: true
     },
     // The window is hidden by default so it can be set full screen if necessary before displaying.
     show: false
   });
 
+  windows.add(window);
+
   // The electron-win-state package doesn't remember full screen state by default
   // so take care of that by reading a saved value from the store.
-  mainWindow.setFullScreen(store.get('fullScreen'));
-  mainWindow.setMenuBarVisibility(!store.get('fullScreen'));
-  mainWindow.show();
+  window.setFullScreen(store.get('fullScreen'));
+  window.setMenuBarVisibility(!store.get('fullScreen'));
+  window.show();
 
   // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  window.loadURL(`${MAIN_WINDOW_WEBPACK_ENTRY}/?screen=${index}`);
 
   // Assume the unpackaged app is being used for dev work and show the dev tools.
   if (!app.isPackaged) {
-    mainWindow.webContents.openDevTools();
+    window.webContents.openDevTools();
   }
 
   // When the app goes into and out of full screen the menu bar visibility gets set and full screen state is
   // is saved so it can be restored on future runs. 
-  mainWindow.on('enter-full-screen', () => { mainWindow.setMenuBarVisibility(false); store.set('fullScreen', true); });
-  mainWindow.on('leave-full-screen', () => { mainWindow.setMenuBarVisibility(true); store.set('fullScreen', false); });
+  window.on('enter-full-screen', () => { window.setMenuBarVisibility(false); store.set('fullScreen', true); });
+  window.on('leave-full-screen', () => { window.setMenuBarVisibility(true); store.set('fullScreen', false); });
+  
+  // Handle closing windows.
+  window.on('closed', () => { windows.delete(window); window = null; });
 
   startSockets();
 };
@@ -146,7 +159,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createWindows);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -161,7 +174,8 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow(1);
+    createWindow(2);
   }
 });
 
